6c2dfd87643a41b280e127090da20765a064fc8e
Hankyul Min <denirokr22@gmail.com>
2023-07-07 13:41:03 +0900

    Init: content-master-service 프로젝트

---
+++ src/main/java/com/travelvcommerce/contentmasterservice/ContentMasterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travelvcommerce.contentmasterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentMasterServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentMasterServiceApplication.class, args);
+    }
+
+}
